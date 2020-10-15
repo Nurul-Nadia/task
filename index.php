7257e236247b332febbb87bc18047c63d0529525
Nurul-Nadia <72897515+Nurul-Nadia@users.noreply.github.com>
2020-10-15 09:29:42 +0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<title>20201014113721</title>
+</head>
+<body>
+What if the link do if let say I pass sleep as the value it should return true, if I pass other keyword than sleep it should return false.
+
+
+<?php
+$target = 'https://giphy.com/embed/xT8qBvH1pAAtsX52u'; // Existing files 
+$link = 'https://192.168.1.11:8181/api/v1/getGif?keyword=sleep'; // Directed to new link
+
+link($target, $link);
+?>
+ 
+</body>
+</html>
